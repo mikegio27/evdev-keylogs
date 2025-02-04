eb1b08da9387a1f360b625bfaac6d6aeb82dcfa7
mike <mike@TGD.mike>
2025-02-03 18:04:05 -0700

    monitor only keyboards

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module evdev-keylogs
 
 go 1.23.5
 
-require github.com/mikegio27/go-evdev v0.0.3
+require github.com/mikegio27/go-evdev v0.0.13
--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/mikegio27/go-evdev"
 )
 
 func main() {
-	dataChanMap, cancel, err := evdev.MonitorDevices(nil)
 	keyMap := evdev.GenerateKeyMap()
+	devices, err := evdev.InputDevices()
 	if err != nil {
-		log.Fatalf("Failed to monitor devices: %v", err)
+		fmt.Println("Error getting input devices:", err)
+		return
+	}
+
+	keyboardDevices := []evdev.InputDevice{}
+	for _, device := range devices {
+		if device.IsKeyboard() {
+			keyboardDevices = append(keyboardDevices, device)
+		}
+	}
+
+	if len(keyboardDevices) == 0 {
+		fmt.Println("No keyboard devices found.")
+		return
+	}
+
+	dataChanMap, cancel, err := evdev.MonitorDevices(keyboardDevices)
+	if err != nil {
+		fmt.Println("Error monitoring devices:", err)
+		return
 	}
 	defer cancel()
+
+	if len(keyboardDevices) == 0 {
+		fmt.Println("No keyboard devices found.")
+		return
+	}
 
 	// Handle the data from the channels
 	for devicePath, dataChan := range dataChanMap {
